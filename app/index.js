5b5f93a96b5cee56f19164555e1554f3f55eaa3d
Benjamim Sonntag <benjamimsonntag@gmail.com>
2016-11-30 12:50:05 +0000

    Refactors NpmNotifier

--- app/index.js
+++ app/index.js
@@ -1,9 +1,27 @@
 var _ = require('lodash')
 var { ipcRenderer, shell } = require('electron')
 
-var NpmFollower = require('../npm-follower')
+var NpmFollower = require('../lib/npm-follower')
 
-NpmFollower(update => {
+var dbUrl = 'https://skimdb.npmjs.com/registry'
+var subscribedPackages = [
+  '*angular*',
+  'eslint*',
+  '*express*',
+  'gulp*',
+  'lodash*',
+  'minimatch',
+  'mongoose',
+  'react*',
+  'request',
+  '*webpack*',
+]
+
+var follower = NpmFollower({ dbUrl })
+
+follower.follow(subscribedPackages)
+
+follower.onUpdate(update => {
   console.log('npm update')
   notifyPackageUpdate(update)
 })
